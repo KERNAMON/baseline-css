cb922ffccf0f2c0b6cb3e2c223d4fee249b5a6a1
Saskia Ostermann Toutatis <kernamon.workstation@gmail.com>
2025-01-07 17:36:03 +0100

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -31,18 +31,22 @@ export async function onRequest(context) {
 
   // Define the metadata
   const ogtag = `
-    <meta property="og:title" content="${metatitle}" />
-    <meta property="og:description" content="${metadescription}" />
-    <meta property="og:locale" content="en_GB" />
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="${request.url}" />
-    <meta property="og:image" content="${request.url}img/preview.png?myQuery=${name}" />
-    <meta property="og:image:height" content="630" />
-    <meta property="og:image:width" content="1200" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:title" content="${metatitle}" />
-    <meta name="twitter:description" content="${metadescription}" />
-    <meta name="description" content="Baseline.CSS is a foundational reset CSS stylesheet aimed at creating a uniform and consistent design across all browsers, mobile-first." />
+    <!-- Open Graph Metadata -->
+    <meta property="og:title" content="Baseline.CSS >> Modern CSS Reset">
+    <meta property="og:description" content="Baseline.css is a foundational CSS reset for consistent styling across modern browsers. Minified for superior Performance.">
+    <meta property="og:url" content="https://baseline-css.pages.dev/">
+    <meta property="og:image" content="https://baseline-css.pages.dev/images/og-image.png">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="en_GB">
+
+    <!-- Twitter Metadata -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Baseline.CSS >> Modern CSS Reset">
+    <meta name="twitter:description" content="Baseline.css is a foundational CSS reset for consistent styling across modern browsers. Minified for superior Performance.">
+    <meta name="twitter:image" content="https://baseline-css.pages.dev/images/og-image.png">
+    <meta name="twitter:site" content="@kernamon">
   `
 
   return new HTMLRewriter().on('head', new ElementHandler(ogtag)).transform(res)
