a2133564a713a208781bf87a3392676ba9b8b519
Saskia Ostermann Toutatis <kernamon.workstation@gmail.com>
2025-01-07 17:31:39 +0100

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -30,8 +30,20 @@ export async function onRequest(context) {
   let name = searchParams.get('myQuery') || 'default'
 
   // Define the metadata
-  ogtag = ``
-  
+  const ogtag = `
+    <meta property="og:title" content="${metatitle}" />
+    <meta property="og:description" content="${metadescription}" />
+    <meta property="og:locale" content="en_GB" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="${request.url}" />
+    <meta property="og:image" content="${request.url}img/preview.png?myQuery=${name}" />
+    <meta property="og:image:height" content="630" />
+    <meta property="og:image:width" content="1200" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="${metatitle}" />
+    <meta name="twitter:description" content="${metadescription}" />
+    <meta name="description" content="Baseline.CSS is a foundational reset CSS stylesheet aimed at creating a uniform and consistent design across all browsers, mobile-first." />
+  `
 
   return new HTMLRewriter().on('head', new ElementHandler(ogtag)).transform(res)
 }
