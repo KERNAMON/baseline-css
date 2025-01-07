9dd9be2c0b197077d396003e0eb009a860a64d42
Saskia Ostermann Toutatis <kernamon.workstation@gmail.com>
2025-01-07 17:05:50 +0100

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -18,13 +18,29 @@ export async function onRequest(context) {
   }
 
   // Metatags Variables (edit these!)
+  const metatitle = "Baseline.CSS"
+  const metadescription = "Baseline.css is a foundational CSS reset for consistent styling across modern browsers. Minified for superior Performance."
 
-  
   let name = searchParams.get('myQuery')
   let ogtag
 
   // these are the metatags we want to inject into the site
-  
+  ogtag = `
+    <meta property="og:title" content="${metatitle}" />
+    <meta property="og:description" content="${metadescription}" />
+    <meta property="og:locale" content="en_GB" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="${request.url}" />
+    <meta property="og:image" content="${request.url}img/preview.png?${name ? 'myQuery=' + name : 'default'}" />
+
+    <meta property="og:image:height" content="630" />
+    <meta property="og:image:width" content="1200" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content="${metatitle}" />
+    <meta name="twitter:description" content="${metadescription}" />
+
+    <meta name="description" content="Baseline.CSS is a foundational reset css stylesheet aimed at creating a uniform and consistent design across all browsers, mobile-first." />
+  `
 
   return new HTMLRewriter().on('head', new ElementHandler(ogtag)).transform(res)
-}
